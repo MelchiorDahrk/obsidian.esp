197c009ee384edc33dc97fcc79d075abfe588e79
MelchiorDahrk <47513579+MelchiorDahrk@users.noreply.github.com>
2026-04-03 00:26:35 -0400

    example output.esp

--- obsidian_md/src/main.rs
+++ obsidian_md/src/main.rs
@@ -51,9 +51,38 @@ fn collect_load_order() -> Vec<PathBuf> {
 fn main() -> Result<()> {
     let _guard = init_logger();
 
-    for path in collect_load_order() {
-        info!("{}", path.display());
+    let example_path = PathBuf::from("examples/obsidianDialogueTest");
+    info!("Parsing markdown from {}...", example_path.display());
+    
+    let parsed = obsidian_md::parse::parse_project_directory(&example_path)?;
+    let original_masters = parsed.header.masters.clone();
+    
+    info!("Collecting load order...");
+    let load_order = collect_load_order();
+    let mut master_paths = Vec::new();
+    let mut original_masters_sized = Vec::new();
+    for m in &original_masters {
+        for p in &load_order {
+            if p.file_name().unwrap().to_str().unwrap().eq_ignore_ascii_case(m) {
+                master_paths.push(p.clone());
+                let size = std::fs::metadata(p).map(|md| md.len()).unwrap_or(0);
+                original_masters_sized.push((m.clone(), size));
+                break; // Found the master
+            }
+        }
     }
+    
+    info!("Compiling into dialogue structures...");
+    let compiled = obsidian_md::compile::compile(parsed)?;
+    
+    info!("Merging into masters and resolving diff...");
+    let resolved = obsidian_md::compile::resolve::resolve(compiled, &master_paths, original_masters_sized)?;
+    
+    let out_path = PathBuf::from("examples/output.esp");
+    info!("Saving into {}...", out_path.display());
+    resolved.save_path(&out_path)?; // Uses inner .into_plugin().save_path()
+
+    info!("Done!");
     Ok(())
 }
 
